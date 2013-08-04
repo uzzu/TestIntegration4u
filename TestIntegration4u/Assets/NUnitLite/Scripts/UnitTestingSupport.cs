804682afb2b740cafee3c9c2528e7bb6d65dab22
uzzu <himeatball@gmail.com>
2013-08-05 05:43:39 +0900

    add unit-testing support extensions

---
+++ TestIntegration4u/Assets/NUnitLite/Scripts/UnitTestingSupport.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Reflection;
+
+namespace NUnitLite.Unity
+{
+	public interface IUnitTestingSupport
+	{
+	}
+	
+	public static class UnitTestingSupport
+	{
+		#region public methods
+		public static void DestroyObject(this IUnitTestingSupport obj, UnityEngine.Object instance)
+		{
+#if UNITY_EDITOR
+			UnityEngine.GameObject.DestroyImmediate(instance);
+#else
+			UnityEngine.GameObject.Destroy(TreeInstance);
+#endif
+		}
+		#endregion
+	}
+}
